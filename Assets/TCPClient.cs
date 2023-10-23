4104575d336f2717d00ad5e90d72b68c10ccfbf9
Isaac Furieri <ibf0024@my.londonmet.ac.uk>
2023-10-23 11:38:50 +0100

    Client Created
    - Created Client
    - Client to Server Connection

--- Assets/TCPClient.cs
+++ Assets/TCPClient.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net.Sockets;
 
 public class TCPClient : MonoBehaviour
 {
+    TcpClient client = null;
+    public string host;
+    public int port;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +17,27 @@ public class TCPClient : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ConnectedToServer();
+        }
+    }
+
+    private void ConnectedToServer()
+    {
+        Debug.Log("Request Connection");
+        client = new TcpClient(host, port);
+    }
+
+    private void SendData() 
+    {
+    
+    }
+
+    private void ReceiveData()
+    {
+
     }
 }
--- Assets/TCPServer.cs
+++ Assets/TCPServer.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Net.Sockets;
 using System.Net;
+using System.Threading;
 
 public class TCPServer : MonoBehaviour
 {
-    public int port;
     public string host;
+    public int port;
+
     private TcpListener server;
+    private Thread serverThread = null;
 
     // Start is called before the first frame update
     void Start()
@@ -20,12 +23,28 @@ public class TCPServer : MonoBehaviour
     // Create Server
     void CreateServer()
     {
+        serverThread = new Thread(ListenerThread);
+        serverThread.Start();
+    }
+
+    void ListenerThread()
+    {
         IPAddress iPAddress = IPAddress.Parse(host);
         server = new TcpListener(iPAddress, port);
         server.Start();
 
         Debug.Log("Server has been created.");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            if (client != null) 
+            {
+                Debug.Log("Client is connected : " + client.Client.LocalEndPoint);
+            }
+        }
     }
+
     // Update is called once per frame
     void Update()
     {
